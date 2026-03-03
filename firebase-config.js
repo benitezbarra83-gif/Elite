ec2c86a7522b513ed90cc7dc0aa937f37c1a79f2
benitezbarra83-gif <benitezbarra83@gmail.com>
2026-03-03 17:25:49 +0000

    Create firebase-config.js file with Firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "elite-f8ffe",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "843499281739",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
